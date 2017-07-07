f6199b4a6a6af1f4a68fa36972929401f54726be
Pubudu Welagedara <pwelagedara@virtusapolaris.com>
2017-07-07 17:14:35 -0400

    configuring

---
+++ Jenkinsfile
@@ -0,0 +1,14 @@
+
+node {
+   stage 'Build'
+   		echo 'Hello World 1'
+   stage 'Staging Deployment'
+   		echo 'Hello World 2'
+        sh 'ls'
+   stage 'Acceptance'
+   		echo 'Hello World 2'
+        sh 'ls'
+   stage 'Production Deployment'
+   		echo 'Hello World 2'
+        sh 'ls'
+}
--- app/app.js
+++ app/app.js
@@ -1,22 +1,8 @@
 var express = require('express');
 var app = express();
 
-/*
-app.get('/hello', function (req, res) {
-  res.send('[GET]Hello World...! from ' + require('os').hostname());
-});
-
-app.get('/p/:tagId', function(req, res) {
-  res.send("tagId is set to " + req.params.tagId);
-});
-*/
-
 app.get('/', function (req, res) {
   res.send('[GET]Hello World! from ' + require('os').hostname());
-});
-
-app.post('/', function(req, res) {
-    res.send('[POST]Hello World! from ' + require('os').hostname());
 });
 
 app.listen(3000, function () {
