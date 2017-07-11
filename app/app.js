ca462431d3cc107dc73fd3b97662c3fe76bae320
Pubudu Welagedara <pwelagedara@virtusapolaris.com>
2017-07-10 21:07:31 -0400

    configuring

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('[GET]Hello World..!!! from ' + require('os').hostname());
+  res.send('[GET]Hello World! from ' + require('os').hostname());
 });
 
 app.listen(3000, function () {
